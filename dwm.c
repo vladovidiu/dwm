ff2b27d30882d99eba531028f08d7d9f8262efce
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2019-06-06 08:36:47 +0100

    center

--- dwm.c
+++ dwm.c
@@ -1474,7 +1474,7 @@ setcentered(const Arg *arg) {
 		selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
 		selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
 	}
-	arrange(selmon)
+	arrange(selmon);
 }
 
 void
