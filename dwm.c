8c910d5e7f683adc97fc50d65f02956b9d03a727
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2019-06-07 08:07:44 +0100

    autostart

--- dwm.c
+++ dwm.c
@@ -196,6 +196,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1422,6 +1423,13 @@ run(void)
 }
 
 void
+runAutostart(void)
+{
+	system("cd ~/.dwm; ./autostart_blocking.sh");
+	system("cd ~/.dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2247,6 +2255,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
 	scan();
 	run();
+	runAutostart();
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
