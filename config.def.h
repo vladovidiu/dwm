8eb853ea88a2c9f1b1d1483901d13c0345c2cd23
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2019-08-05 07:45:34 +0100

    update packages

--- config.def.h
+++ config.def.h
@@ -5,13 +5,20 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:size=10" };
-static const char dmenufont[]       = "Fira Code:size=10";
+static const char *fonts[]          = { "UbuntuMono Nerd Font:size=11" };
+static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
+/* static const char dmenufont[]       = "Fira Code:size=10"; */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+/* static const char col_gray1[]       = "#1d2330"; */
+/* static const char col_gray2[]       = "#4b5665"; /1* border color unfocused windows *1/ */
+/* static const char col_gray3[]       = "#ad69af"; */
+/* static const char col_gray4[]       = "#d1d7e1"; */
+/* static const char col_cyan[]        = "#84598d"; /1* border color focused windows and tags *1/ */
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,6 +27,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,7 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[]    = { "dmenu_run", NULL };
-static const char *termcmd[]     = { "alacritty", NULL };
+static const char *termcmd[]     = { "st", NULL };
 static const char *clipmenucmd[] = { "clipmenu", NULL };
 static const char *pri2clip[]  = { "sh", "-c", "xclip -o -selection primary | xclip -selection clipboard", NULL };
 
